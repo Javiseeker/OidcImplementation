585ee4768cf4af4ce62488a287a26163b1485c7a
Javiseeker <javieralonsol@churchofjesuschrist.org>
2025-06-23 10:44:30 +0100

    added 2 sample projects to repo, minimal api and mvc

---
+++ src/OidcImplementation.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OidcImplementation.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
